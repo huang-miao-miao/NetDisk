8799cb8aea688c5f352407ce91ed998d1fd40e99
zheng <hello@zheng.cn>
2024-09-22 16:22:14 +0800

    init

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login-body">
+    <div class="bg"></div>
+    <div class="login-panel"></div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+  .login-body {
+    height: calc(100vh);
+    background-size: cover;
+    background: url("");
+  }
+</style>
